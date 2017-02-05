610bac1843e8fad879b058884b8086a0469988e2
Morhaus <alexandre.kirszenberg@gmail.com>
2017-02-05 23:42:23 +0200

    Add assignment 2 and submissions

---
+++ README.md
@@ -0,0 +1,11 @@
+# gene-expression-prediciton
+
+Repository for the group 32:
+* Raúl Berganza Gómez (262847) <raul.berganzagomez@student.tut.fi>,
+* Joanna Polewczyk (270715) <joanna.polewczyk@student.tut.fi>
+* Alexandre Kirszenberg (270743) <alexandre.kirszenberg@student.tut.fi>
+* Antoine Sainson (270744) <antoine.sainson@student.tut.fi>
+
+## Submission history
+
+* **0.89604**: `LogisticRegression(C=10.0 ** -2, penalty="l1")`
--- assig2.py
+++ assig2.py
@@ -9,6 +9,7 @@ import itertools
 print "Loading data"
 x_train_raw = np.load("data/x_train.npy")
 y_train_raw = np.load("data/y_train.npy")
+x_test_raw = np.load("data/x_test.npy")
 
 # Pack into 500 length arrays of histone modification signals
 # No idea why, but having a shape of n * 500 instead of (n * 100) * 5 improves
@@ -16,10 +17,16 @@ y_train_raw = np.load("data/y_train.npy")
 # With Deep Learning, we keep the (n * 100) * 5
 x_train = [e.ravel() for e in np.split(x_train_raw[:, 1:], x_train_raw.shape[0] / 100)]
 y_train = y_train_raw[:, 1:].ravel()
+x_test = [e.ravel() for e in np.split(x_test_raw[:, 1:], x_test_raw.shape[0] / 100)]
 
-for C, penalty in itertools.product(10.0 ** np.arange(-6, 1), ["l1", "l2"]):
-    print "Training model with C=%.2e, penalty=%s" % (C, penalty)
-    clf = LogisticRegression(C=C, penalty=penalty)
-    clf.fit(X_train, Y_train)
-    scores = cross_val_score(clf, x_train, y_train, scoring="roc_auc")
-    print "Accuracy: %0.3f (+/- %0.3f)" % (scores.mean(), scores.std() * 2)
+# From assig3.py
+clf = LogisticRegression(C=10.0 ** -2, penalty="l1")
+clf.fit(x_train, y_train)
+# Only take the probability of the gene being active.
+predictions = clf.predict_proba(x_test)[:,1].ravel()
+
+fp = open("prediction.csv", "w")
+fp.write("GeneId,Prediction\n")
+for idx, prediction in enumerate(predictions):
+    fp.write("%i,%f\n" % ((idx + 1), prediction))
+fp.close()
