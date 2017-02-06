c344b1c93864c48f9c61e00aa1b1615683bac9dc
Morhaus <alexandre.kirszenberg@gmail.com>
2017-02-06 14:50:03 +0200

    Correctly bound y axis

--- plot_gene.py
+++ plot_gene.py
@@ -12,7 +12,9 @@ x_train = np.split(x_train_raw[:, 1:], x_train_raw.shape[0] / 100)
 y_train = y_train_raw[:, 1:].ravel()
 x_test = np.split(x_test_raw[:, 1:], x_test_raw.shape[0] / 100)
 
+y_max = np.amax(x_train)
+
 def plot_gene(i):
-    plt.title(y_train[i])
-    plt.ylim(0, 100)
+    plt.title("%i: %i" % (i, y_train[i]))
+    plt.ylim(0, y_max)
     plt.plot(x_train[i])
